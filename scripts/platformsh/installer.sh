7015d0492e1b42e10ad1481fa7c695c2a6fa367d
Ariel Barreiro <abarrei@gmail.com>
2024-02-26 19:13:13 -0300

    PLATFORMSH_RECIPES_INSTALLDIR env var

--- scripts/platformsh/installer.sh
+++ scripts/platformsh/installer.sh
@@ -20,14 +20,16 @@ done
 ##
 # Get the content of https://github.com/hanoii/platformsh-recipes.
 ###
+PLATFORMSH_RECIPES_INSTALLDIR=${PLATFORMSH_RECIPES_INSTALLDIR-$PLATFORM_APP_DIR/.platformsh-recipes}
 echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing hanoii/platformsh-recipes...\033[0m"
-mkdir -p .platformsh-recipes
-wget -qO- https://github.com/hanoii/platformsh-recipes/archive/${PLATFORMSH_RECIPES_VERSION}.tar.gz | tar -zxf - --strip-component=1 -C .platformsh-recipes
-echo "${PLATFORMSH_RECIPES_VERSION}" > .platformsh-recipes/version
+mkdir -p $PLATFORMSH_RECIPES_INSTALLDIR
+wget -qO- https://github.com/hanoii/platformsh-recipes/archive/${PLATFORMSH_RECIPES_VERSION}.tar.gz | tar -zxf - --strip-component=1 -C $PLATFORMSH_RECIPES_INSTALLDIR
+echo "${PLATFORMSH_RECIPES_VERSION}" > $PLATFORMSH_RECIPES_INSTALLDIR/version
 if [[ -n "$full" ]]; then
   # Install tools
   ./.platformsh-recipes/scripts/platformsh-recipes/platformsh/build.sh
-  echo "source $PLATFORM_APP_DIR/.platformsh-recipes/scripts/platformsh/.environment" >> $PLATFORM_APP_DIR/.environment
-  echo "source $PLATFORM_APP_DIR/.platformsh-recipes/scripts/platformsh/.bashrc" >> $PLATFORM_APP_DIR/.bashrc
+  echo "export PLATFORMSH_RECIPES_INSTALLDIR=$$PLATFORMSH_RECIPES_INSTALLDIR" >> $PLATFORM_APP_DIR/.environment
+  echo "source $PLATFORMSH_RECIPES_INSTALLDIR/scripts/platformsh/.environment" >> $PLATFORM_APP_DIR/.environment
+  echo "source $PLATFORMSH_RECIPES_INSTALLDIR/.platformsh-recipes/scripts/platformsh/.bashrc" >> $PLATFORM_APP_DIR/.bashrc
 fi
 echo -e "\033[0;32m[$(date -u "+%Y-%m-%d %T.%3N")] Done installing hanoii/platformsh-recipes!\n\033[0m"
