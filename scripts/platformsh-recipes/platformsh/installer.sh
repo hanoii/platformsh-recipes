36353236b930636f7a2785ce31c57505ed4c7a01
Ariel Barreiro <abarrei@gmail.com>
2024-02-26 16:06:48 -0300

    installer.sh comment move

--- scripts/platformsh-recipes/platformsh/installer.sh
+++ scripts/platformsh-recipes/platformsh/installer.sh
@@ -18,8 +18,8 @@ done
 echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing hanoii/platformsh-recipes...\033[0m"
 mkdir -p .platformsh-recipes
 wget -qO- https://github.com/hanoii/platformsh-recipes/archive/de73c72459a81a4bcb08554a9b7e5a53a9d48edc.tar.gz | tar -zxf - --strip-component=1 -C .platformsh-recipes
-# Install tools
 if [[ -n "$full" ]]; then
+  # Install tools
   ./.platformsh-recipes/scripts/platformsh-recipes/platformsh/build.sh
   echo "source $PLATFORM_APP_DIR/scripts/platformsh-recipes/platformsh/.environment" >> $PLATFORM_APP_DIR/.environment
   echo "source $PLATFORM_APP_DIR/scripts/platformsh-recipes/platformsh/.bashrc" >> $PLATFORM_APP_DIR/.bashrc
