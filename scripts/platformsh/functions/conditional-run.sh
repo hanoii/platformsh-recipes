b259d95d22d9d772941a52fe8a661ef6a88dbf16
Ariel Barreiro <abarrei@gmail.com>
2024-04-09 12:41:33 -0300

    cr drupal preset

--- scripts/platformsh/functions/conditional-run.sh
+++ scripts/platformsh/functions/conditional-run.sh
@@ -50,7 +50,11 @@ platformsh_recipes_cr_success() {
 }
 
 platformsh_recipes_cr_cleanup() {
-  find "${PLATFORM_CACHE_DIR}/platformsh-recipes/cr" -mindepth 2 -type d -mtime +15 -exec rm -rf {} +
+  local finddir="${PLATFORM_CACHE_DIR}/platformsh-recipes/cr"
+  if [ -n "$1" ]; then
+    finddir="$finddir/$1"
+  fi
+  find "$finddir" -mindepth 2 -type d -mtime +15 -exec rm -rf {} +
 }
 
 platformsh_recipes_cr_deploy_store() {
@@ -61,4 +65,40 @@ platformsh_recipes_cr_deploy_store() {
 
   find $1 -maxdepth 1 -name .platformsh-recipes.hash\.\* -not -name \*.bak -exec cp {} {}.bak \;
   cp .platformsh-recipes.hash.* $1
+}
+
+
+### Presets
+
+# Conditionally build and cache composer assets for drupal
+platformsh_recipes_cr_preset_drupal_composer() {
+  local hashfiles="composer.*"
+  # Some directories normally present
+  if [ -d patches/ ]; then
+    hashfiles="$hashfiles patches/"
+  fi
+  if [ -d PATCHES/ ]; then
+    hashfiles="$hashfiles PATCHES/"
+  fi
+  platformsh_recipes_cr_init "composer" \
+    $(echo $PLATFORM_APPLICATION | base64 -d | jq '.type') \
+    $hashfiles
+
+  local cache_dir=$(platformsh_recipes_cr_get_cache_dir "composer")
+  if platformsh_recipes_cr_should_run "composer"; then
+    echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Composer install...\033[0m"
+    composer global require composer/composer:^2
+    composer install --no-interaction --no-dev
+    tar -czf ${cache_dir}/cache.tar.gz $(ls -d vendor/ web/core web/modules/contrib web/libraries web/themes/contrib web/profiles/contrib drush/Commands/contrib 2>/dev/null)
+    platformsh_recipes_cr_success "composer"
+    echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Composer cache stored!!\033[0m"
+    ls -lha ${cache_dir}/cache.tar.gz
+  else
+    echo -e "\033[0;33m[$(date -u "+%Y-%m-%d %T.%3N")] [warning] Using composer dependencies from cache...\033[0m"
+    tar -zxf ${cache_dir}/cache.tar.gz
+    ls -lha ${cache_dir}/cache.tar.gz
+    echo -e "\033[0;32m[$(date -u "+%Y-%m-%d %T.%3N")] Done using composer dependencies from cache!\033[0m"
+  fi
+
+  platformsh_recipes_cr_cleanup "composer"
 }
