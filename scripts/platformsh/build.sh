4e219bdb8f214a6acde6faa264c4526acff9931e
Ariel Barreiro <abarrei@gmail.com>
2024-04-28 19:35:08 -0300

    Support both debian 10 and debian 11
    Refactor code and added kitty terminfo as a debian package

--- scripts/platformsh/build.sh
+++ scripts/platformsh/build.sh
@@ -1,69 +1,47 @@
 #!/bin/bash
 set -e
 
-mkdir -p .global/bin
+function install_debian() {
+  mkdir -p $PLATFORM_APP_DIR/.global/bin
+  mkdir -p $PLATFORM_APP_DIR/.global/lib
+  mkdir -p $PLATFORM_APP_DIR/.global/terminfo
+  local pkg=$1
+  echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing $pkg...\033[0m"
+  local debian_version=
+  read -d . debian_version < /etc/debian_version
+  local pkgs=($(cat $PLATFORMSH_RECIPES_INSTALLDIR/scripts/platformsh/debian.json | jq -r ".\"$pkg\" | .\"$debian_version\" | .[]" 2> /dev/null))
 
-##
-# Install screen package and its dependencies
-###
-echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing screen...\033[0m"
-mkdir -p /tmp/screen
-cd /tmp/screen
-wget -q http://ftp.us.debian.org/debian/pool/main/s/screen/screen_4.6.2-3+deb10u1_amd64.deb
-ar x screen_4.6.2-3+deb10u1_amd64.deb
-tar -xf data.tar.xz
-cd
-mv /tmp/screen/usr/bin/screen .global/bin
-rm -fr /tmp/screen
-mkdir -p /tmp/libutempter0
-cd /tmp/libutempter0
-wget -q http://ftp.us.debian.org/debian/pool/main/libu/libutempter/libutempter0_1.1.6-3_amd64.deb
-ar x libutempter0_1.1.6-3_amd64.deb
-tar -xf data.tar.xz
-cd
-cp -R /tmp/libutempter0/usr/lib .global
-rm -fr /tmp/libutempter0
+  if [ -z $pkgs ]; then
+    echo -e "\033[0;33m[$(date -u "+%Y-%m-%d %T.%3N")] [warning] no known packages to download for $pkg, it wasn't installed.\033[0m"
+    return 0
+  fi
+  for i in "${!pkgs[@]}"; do
+    mkdir -p /tmp/$pkg$i
+    cd /tmp/$pkg$i
+    wget -q "${pkgs[$i]}" -O $pkg$i.deb
+    ar x $pkg$i.deb
+    tar -xf data.tar.xz
+    [ -d usr/bin ] && cp -R usr/bin/* $PLATFORM_APP_DIR/.global/bin
+    [ -d lib ] && cp -R lib/* $PLATFORM_APP_DIR/.global/lib
+    [ -d usr/lib ] && cp -R usr/lib/* $PLATFORM_APP_DIR/.global/lib
+    [ -d usr/share/terminfo ] && cp -R usr/share/terminfo/* $PLATFORM_APP_DIR/.global/terminfo
+    cd
+    rm -fr /tmp/$pkg$i
+  done
+}
+
+# Install debian packages manually
+install_debian ansilove
+install_debian colorized-logs
+install_debian htop
+install_debian kitty-terminfo
+install_debian logrotate
+install_debian pv
+install_debian screen
+
+# screen tweaks
 echo "defscrollback 1000000" >> ~/.screenrc
 echo 'shell -$SHELL' >> ~/.screenrc
-
-##
-# Install pv
-###
-echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing pv...\033[0m"
-mkdir -p /tmp/pv
-cd /tmp/pv
-wget -q http://ftp.us.debian.org/debian/pool/main/p/pv/pv_1.6.6-1_amd64.deb
-ar x pv_1.6.6-1_amd64.deb
-tar -xf data.tar.xz
-cd
-mv /tmp/pv/usr/bin/pv .global/bin
-rm -fr /tmp/pv
-
-##
-# Install htop
-###
-echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing htop...\033[0m"
-mkdir -p /tmp/htop
-cd /tmp/htop
-wget -q http://http.us.debian.org/debian/pool/main/h/htop/htop_2.2.0-1+b1_amd64.deb
-ar x htop_2.2.0-1+b1_amd64.deb
-tar -xf data.tar.xz
-cd
-mv /tmp/htop/usr/bin/htop .global/bin
-rm -fr /tmp/htop
-
-##
-# Install logrotate
-###
-echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing logrotate...\033[0m"
-mkdir -p /tmp/logrotate
-cd /tmp/logrotate
-wget -q http://ftp.us.debian.org/debian/pool/main/l/logrotate/logrotate_3.14.0-4_amd64.deb
-ar x logrotate_3.14.0-4_amd64.deb
-tar -xf data.tar.xz
-cd
-mv /tmp/logrotate/usr/sbin/logrotate .global/bin
-rm -fr /tmp/logrotate
 
 # Install fzf
 echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing fzf...\033[0m"
@@ -74,8 +52,6 @@ echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing platform cli...\03
 echo "Installing Platform.sh CLI"
 curl -fsSL https://raw.githubusercontent.com/platformsh/cli/main/installer.sh | bash > /dev/null
 
-##
 # Install ahoy
-###
 echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing ahoy...\033[0m"
 wget -q https://github.com/ahoy-cli/ahoy/releases/download/v2.1.1/ahoy-bin-linux-amd64 -O .global/bin/ahoy && chmod +x .global/bin/ahoy
