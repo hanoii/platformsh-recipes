0211f8993cd12b1859d296e3c3bdf8e37cffb3c2
Ariel Barreiro <abarrei@gmail.com>
2024-05-10 11:23:37 -0300

    feat: make installer.sh sourceable

--- installer.sh
+++ installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Platform.sh recipes installer
 
+set -e -o pipefail
+
 if [[ -z "$PLATFORMSH_RECIPES_VERSION" ]]; then
   >&2 echo -e "\033[0;31m[error] PLATFORMSH_RECIPES_VERSION not provided!\033[0m"
   exit 1
@@ -10,8 +12,6 @@ if [[ -z "$PLATFORM_APP_DIR" ]]; then
   >&2 echo -e "\033[0;31m[error] This script is meant to be run on a Platform.sh environment!\033[0m"
   exit 1
 fi
-
-set -euo pipefail
 
 full=
 while getopts "f" option; do
@@ -27,21 +27,32 @@ done
 ###
 PLATFORMSH_RECIPES_INSTALLDIR=${PLATFORMSH_RECIPES_INSTALLDIR-$PLATFORM_APP_DIR/.platformsh-recipes}
 echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing hanoii/platformsh-recipes...\033[0m"
+
+if [ -n "$IS_DDEV_PROJECT" ] && [ -L $PLATFORMSH_RECIPES_INSTALLDIR ]; then
+  rm $PLATFORMSH_RECIPES_INSTALLDIR
+fi
+
 mkdir -p $PLATFORMSH_RECIPES_INSTALLDIR
 wget -qO- https://github.com/hanoii/platformsh-recipes/archive/${PLATFORMSH_RECIPES_VERSION}.tar.gz | tar -zxf - --strip-component=1 -C $PLATFORMSH_RECIPES_INSTALLDIR
+
+if [ -n "$IS_DDEV_PROJECT" ]; then
+  rm -fr $PLATFORMSH_RECIPES_INSTALLDIR
+  ln -fs ../ $PLATFORMSH_RECIPES_INSTALLDIR
+fi
+
 echo "${PLATFORMSH_RECIPES_VERSION}" > $PLATFORMSH_RECIPES_INSTALLDIR/version
 if [[ -n "$full" ]]; then
   # Install tools
-  export PLATFORMSH_RECIPES_INSTALLDIR
   $PLATFORMSH_RECIPES_INSTALLDIR/assets/platformsh/build.sh
   echo "export PLATFORMSH_RECIPES_INSTALLDIR=$PLATFORMSH_RECIPES_INSTALLDIR" >> $PLATFORM_APP_DIR/.environment
   # Using . instead of source so it's dash-compatible.
   echo ". $PLATFORMSH_RECIPES_INSTALLDIR/assets/platformsh/.environment" >> $PLATFORM_APP_DIR/.environment
 fi
 echo -e "\033[0;32m[$(date -u "+%Y-%m-%d %T.%3N")] Done installing hanoii/platformsh-recipes!\n\033[0m"
-ln -s $PLATFORMSH_RECIPES_INSTALLDIR/.ahoy.platformsh-recipes.yml $PLATFORM_APP_DIR/
 
 _latest=$(curl -s "https://api.github.com/repos/hanoii/platformsh-recipes/commits/main" | jq -r '.sha')
 if [[ "$PLATFORMSH_RECIPES_VERSION" != "$_latest" ]]; then
   >&2 echo -e "\033[0;33m[warning] You are not using the latest version: '${_latest}'.\033[0m"
 fi
+
+source $PLATFORMSH_RECIPES_INSTALLDIR/assets/platformsh/functions.sh
