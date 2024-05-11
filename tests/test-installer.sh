212b60f303d39a3a624189bd6f22196c269d04c4
Ariel Barreiro <abarrei@gmail.com>
2024-05-10 23:29:09 -0300

    fix: test-installer with PLATFORMSH_RECIPES_INSTALLDIR

--- tests/test-installer.sh
+++ tests/test-installer.sh
@@ -2,6 +2,7 @@
 
 export PLATFORMSH_RECIPES_VERSION=808de168ca52fe727106af30f67e3699fae8f2b9
 export PLATFORM_APP_DIR=/var/www/html/tmp
+export PLATFORMSH_RECIPES_INSTALLDIR=/var/www/html/tmp/.platformsh-recipes
 source /dev/stdin  <<< "$(cat installer.sh)" -f
 
 platformsh_recipes_cr_get_cache_dir
