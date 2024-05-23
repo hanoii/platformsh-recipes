c29d261ee98009fa41f991eb0944141294c8ef82
Ariel Barreiro <abarrei@gmail.com>
2024-05-23 00:20:48 -0300

    feat: support debian snapshot for some packages
    For now just creen as an example

--- platformsh-recipes/assets/platformsh/build.sh
+++ platformsh-recipes/assets/platformsh/build.sh
@@ -24,56 +24,75 @@ case $arch in
     ;;
 esac
 
-function install_debian() {
+function install_debian_url() {
   mkdir -p $PLATFORM_APP_DIR/.global/bin
   mkdir -p $PLATFORM_APP_DIR/.global/lib
   mkdir -p $PLATFORM_APP_DIR/.global/share
-  echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing debian $@ packages...\033[0m"
 
   for i in "$@"; do
-    local curl_cmd="curl --connect-timeout 5 --retry 10 --retry-delay 1 -sS https://packages.debian.org/${VERSION_CODENAME_OVERRIDE-$VERSION_CODENAME}/${VERSION_ARCH_OVERRIDE-$VERSION_ARCH}/$i/download"
-    if [ -n "$PLATFORMSH_RECIPES_DEBUG" ]; then
-      echo -e "\033[0;36m[debug] $curl_cmd\033[0m"
-    fi
-    local pkg_url=$($curl_cmd | grep -oP 'http://http.us.debian.org/debian/pool/main/.*?\.deb')
-    echo "Installing $(basename $pkg_url)..."
-    mkdir -p /tmp/$i
-    cd /tmp/$i
-    wget -q "$pkg_url" -O $i.deb
-    ar x $i.deb
+    local pkg_url=$i
+    local pkg_name=$(basename $pkg_url)
+    echo "Installing ${pkg_name}..."
+    mkdir -p /tmp/${pkg_name}
+    cd /tmp/${pkg_name}
+    wget -q "$pkg_url" -O "${pkg_name}"
+    ar x ${pkg_name}
     tar -xf data.tar.xz
     [ -d usr/bin ] && cp -R usr/bin/* $PLATFORM_APP_DIR/.global/bin
     [ -d lib ] && cp -R lib/* $PLATFORM_APP_DIR/.global/lib
     [ -d usr/lib ] && cp -R usr/lib/* $PLATFORM_APP_DIR/.global/lib
     [ -d usr/share ] && cp -R usr/share/* $PLATFORM_APP_DIR/.global/share
     cd
-    rm -fr /tmp/$i
+    rm -fr /tmp/${pkg_name}
   done
 }
 
-if [ "$VERSION_CODENAME" != "stretch" ]; then
-  # Install debian packages manually
-  VERSION_CODENAME_OVERRIDE=${VERSION_CODENAME//buster/bullseye} install_debian ansilove
-  install_debian colorized-logs
-  install_debian htop libnl-3-200 libnl-genl-3-200
-  VERSION_CODENAME_OVERRIDE=unstable VERSION_ARCH_OVERRIDE=all install_debian kitty-terminfo
-  install_debian logrotate
-  install_debian pv
-  install_debian screen libutempter0
-  install_debian telnet
-  install_debian vim-nox vim-runtime libgpm2 liblua5.2-0
-  if [ -f $PLATFORM_APP_DIR/.global/bin/telnet.netkit ]; then
-    mv $PLATFORM_APP_DIR/.global/bin/telnet.netkit $PLATFORM_APP_DIR/.global/bin/telnet
-  fi
-  if [ -f $PLATFORM_APP_DIR/.global/bin/vim.nox ]; then
-    mv $PLATFORM_APP_DIR/.global/bin/vim.nox $PLATFORM_APP_DIR/.global/bin/vi
-  fi
+function install_debian() {
+  echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing debian $@ packages...\033[0m"
+  local codename=${VERSION_CODENAME_OVERRIDE-$VERSION_CODENAME}
+  local arch=${VERSION_ARCH_OVERRIDE-$VERSION_ARCH}
 
-  # screen tweaks
-  cp $PLATFORMSH_RECIPES_INSTALLDIR/platformsh-recipes/assets/platformsh/.screenrc ~/.screenrc
-else
-  >&2 echo -e "\033[0;33m[warning] installing debian packages for $VERSION_CODENAME is no longer supported.'.\033[0m"
+  for i in "$@"; do
+    local pkg_url=
+    if [ "$codename" != "stretch" ]; then
+      local curl_cmd="curl --connect-timeout 5 --retry 10 --retry-delay 1 -sS https://packages.debian.org/$codename/$arch/$i/download"
+      if [ -n "$PLATFORMSH_RECIPES_DEBUG" ]; then
+        echo -e "\033[0;36m[debug] $curl_cmd\033[0m"
+      fi
+      pkg_url=$($curl_cmd | grep -oP 'http://http.us.debian.org/debian/pool/main/.*?\.deb')
+    fi
+
+    if [ "$codename" = "stretch" ] && [ "$i" = "screen" ]; then
+      pkg_url="https://snapshot.debian.org/archive/debian-archive/20240331T102506Z/debian-security/pool/updates/main/s/screen/screen_4.5.0-6%2Bdeb9u1_$arch.deb"
+    fi
+
+    if [ -z "$pkg_url" ]; then
+      >&2 echo -e "\033[0;33m[warning] installing debian package $i for $codename is currently not supported.\033[0m"
+    else
+      install_debian_url $pkg_url
+    fi
+  done
+}
+
+# Install debian packages manually
+VERSION_CODENAME_OVERRIDE=unstable VERSION_ARCH_OVERRIDE=all install_debian kitty-terminfo
+VERSION_CODENAME_OVERRIDE=${VERSION_CODENAME//buster/bullseye} install_debian ansilove
+install_debian colorized-logs
+install_debian htop libnl-3-200 libnl-genl-3-200
+install_debian logrotate
+install_debian pv
+install_debian screen libutempter0
+install_debian telnet
+install_debian vim-nox vim-runtime libgpm2 liblua5.2-0
+if [ -f $PLATFORM_APP_DIR/.global/bin/telnet.netkit ]; then
+  mv $PLATFORM_APP_DIR/.global/bin/telnet.netkit $PLATFORM_APP_DIR/.global/bin/telnet
 fi
+if [ -f $PLATFORM_APP_DIR/.global/bin/vim.nox ]; then
+  mv $PLATFORM_APP_DIR/.global/bin/vim.nox $PLATFORM_APP_DIR/.global/bin/vi
+fi
+
+# screen tweaks
+cp $PLATFORMSH_RECIPES_INSTALLDIR/platformsh-recipes/assets/platformsh/.screenrc ~/.screenrc
 
 # Install fzf
 echo -e "\033[0;36m[$(date -u "+%Y-%m-%d %T.%3N")] Installing fzf...\033[0m"
