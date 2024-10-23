7d6bec2297c4d9dee3eec97652c615fb7d53c2f7
Ariel Barreiro <abarrei@gmail.com>
2024-10-23 16:01:18 -0300

    chore(ddev): addons update, including self

--- .ddev/platformsh-recipes/scripts/commands/ipblock-remove.sh
+++ .ddev/platformsh-recipes/scripts/commands/ipblock-remove.sh
@@ -2,9 +2,40 @@
 #ddev-generated
 set -e -o pipefail
 
+USAGE=$(cat << EOM
+Usage: ahoy ... ipblock:rm [options] [ip-to-unblock]
+
+  -h                This help text.
+  -r                Run the command instead of printing it
+  -f                Force whitelisting the IP even if it was not previously blocked, basically preemptively whitelisting it.
+EOM
+)
+
+function print_help() {
+  gum style --border "rounded" --margin "1 2" --padding "1 2" "$@" "$USAGE"
+}
+
+run=
+force=
+while getopts ":hfr" option; do
+  case ${option} in
+    h)
+      print_help
+      exit 0
+      ;;
+    r)
+      run=true
+      ;;
+    f)
+      force=true
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
 # The command sets all of the IP addresses path, this is a helper command so
 # that one can add a new ip to the list, it will not do anything, only build the
-# command for you to run it.
+# command for you to run it unless you pass -r
 
 if [[ -z "$1" ]]; then
   echo -e "You need to provide an ip to remove!"
@@ -13,7 +44,7 @@ fi
 
 httpaccess=$(platform httpaccess -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} 2>&1)
 # { grep deny || test $? = 1; } from https://stackoverflow.com/a/49627999
-blocked_already=$(echo "$httpaccess" 2>&1 | { grep deny || test $? = 1; } | perl -pe "s/.*?address: ([^\s]*).*/\$1/")
+blocked_already=$(echo "$httpaccess" 2>&1 | { grep deny || test $? = 1; } | perl -pe "s/.*?address: '?([^\s']*).*/\$1/")
 
 OLDIFS=$IFS
 IFS=$'\n'
@@ -26,15 +57,32 @@ for i in ${!ips_array[*]}; do
   if [[ ! "${ips_array[$i]}" =~ ^"$1" ]]; then
     block="$block --access deny:${ips_array[$i]}"
   else
-    found="${found}IP '$1' matched ${ips_array[$i]}, will be removed!\n"
+    found="${found}IP '$1' is blocked by ${ips_array[$i]}\n"
   fi
 done
 
-if [ -z $found ]; then
+whitelist_filename=.deploy/.platformsh-recipes/ipblock.whitelist
+
+if [ -z "$found" ]; then
   >&2 echo -e "\033[0;32m\nNo IP matching $1 was found!\n\033[0m"
+  if [ "$force" == "true" ]; then
+    gum log --level info "Whitelisting $1 anyway as -f was provided..."
+    platform ssh -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} "echo $1 >> \$PLATFORM_APP_DIR/${whitelist_filename}"
+  fi
 else
-  >&2 echo -e "\033[0;36m\nIf you want to remove the IPs matching $1 from the currently blocked IPs, run the following...\n\033[0m"
-  >&2 echo -e "platform httpaccess -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master}\\\\\n\t\t$block"
+  if [ "$run" == "true" ]; then
+    gum log --level info "Whitelisting $1..."
+    platform ssh -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} "echo $1 >> \$PLATFORM_APP_DIR/${whitelist_filename}"
+    gum log --level info "Removing $1 from the deny list..."
+    platform httpaccess -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} $block
+  else
+    >&2 echo -e "\033[0;36m\nIf you want to remove the IPs matching $1 from the currently blocked IPs, run the following...\n\033[0m"
+    >&2 echo -e "platform httpaccess -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master}\\\\\n\t\t$block"
+
+    >&2 echo -e "\033[0;36m\nYou need to whitelist $1 before by running the following:\n\033[0m"
+    >&2 echo -e "platform ssh -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} \"echo $1 >> \\\$PLATFORM_APP_DIR/${whitelist_filename}\""
+    >&2 echo -e "\033[0;33m\nAltenatively, you can run the same command but adding '-r' before the IP.\033[0m"
+  fi
 
   >&2 echo -e "\033[0;32m\n$found\033[0m"
 
--- .ddev/platformsh-recipes/scripts/logs/ipblock.sh
+++ .ddev/platformsh-recipes/scripts/logs/ipblock.sh
@@ -35,11 +35,23 @@ ips_array=($ips)
 IFS=$OLDIFS
 block=""
 echo ""
+
+whitelist=
+whitelist_filename=$PLATFORM_APP_DIR/.deploy/.platformsh-recipes/ipblock.whitelist
+echo "$whitelist_filename"
+if [ -f "$whitelist_filename" ]; then
+  whitelist=$(cat "$whitelist_filename" | grep -v -e "^#")
+fi
+
 for i in ${!ips_array[*]}; do
   ip_cnt=(${ips_array[$i]})
   if [ ${ip_cnt[0]} -gt ${PLATFORMSH_RECIPES_IPBLOCK_THRESHOLD-20} ]; then
     ip=${ip_cnt[1]//[[:space:]]/}
     if [[ ! "${blocked_ips[@]}" =~ $ip ]]; then
+      if [[ "${whitelist[@]}" =~ $ip ]]; then
+        >&2 echo -e "\033[0;33m[ warn  ] Bad IP $ip found with ${ip_cnt[0]} hits, but it is whitelisted...\033[0m"
+        continue
+      fi
       >&2 echo -e "\033[0;33m[       ] Bad IP $ip found with ${ip_cnt[0]} hits, about to be blocked...\033[0m"
       if [[ "$ip" =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ ]]; then
         block="$block --access deny:$ip/32"
@@ -47,6 +59,9 @@ for i in ${!ips_array[*]}; do
         block="$block --access deny:$ip/128"
       fi
     else
+      if [[ "${whitelist[@]}" =~ $ip ]]; then
+        >&2 echo -e "\033[0;31m[ error ] Bad IP $ip found with ${ip_cnt[0]} hits was blocked, but it is now whitelisted, please remove.\033[0m"
+      fi
       >&2 echo -e "\033[0;32m[ ok    ] Bad IP $ip found with ${ip_cnt[0]} hits, but already blocked...\033[0m"
     fi
   fi
