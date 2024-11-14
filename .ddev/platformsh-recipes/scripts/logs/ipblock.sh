56ef2778658e03859e18165f9da80adec2705079
Ariel Barreiro <abarrei@gmail.com>
2024-11-14 11:04:33 -0300

    feat: ipblock supports passing an ip
    This allows to show the urls that made the IP being blocked

--- .ddev/platformsh-recipes/scripts/logs/ipblock.sh
+++ .ddev/platformsh-recipes/scripts/logs/ipblock.sh
@@ -16,19 +16,71 @@ if [[ -z "$PLATFORMSH_RECIPES_IPBLOCK_WHITELIST" ]]; then
   >&2 echo -e "\033[0;33mWarning: PLATFORMSH_RECIPES_IPBLOCK_WHITELIST not available, this could be fine.\033[0m"
 fi
 
-httpaccess=$(platform httpaccess -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} 2>&1)
-# { grep deny || test $? = 1; } from https://stackoverflow.com/a/49627999
-blocked_ips=($(echo "$httpaccess" | { grep deny || test $? = 1; }  | perl -pe "s/.*?address: '?([^\s\/]*).*/\$1/" | xargs -I {} echo '{}' | xargs))
-blocked_already=$(echo "$httpaccess" | { grep deny || test $? = 1; }  | perl -pe "s/.*?address: ([^\s]*).*/\$1/" | xargs -I {} echo '--access deny:{}' | xargs)
+OPTIONS=`getopt -o '' -l ips-only,days:,ip: -- "$@"`
+eval set -- "$OPTIONS"
+
+iponly=
+ip=
+days=
+httpaccess=1
+# extract options and their arguments into variables.
+while true ; do
+  case "$1" in
+    --ip)
+      httpaccess=
+      ip=$2
+      shift 2
+      ;;
+    --ips-only)
+      httpaccess=
+      iponly=1
+      shift
+      ;;
+    --days)
+      case "$2" in
+        "") shift 2 ;;
+        *)
+          days="--days $2"
+          shift 2 ;;
+      esac ;;
+    --) shift ; break ;;
+    *) echo "Internal error!" ; exit 1 ;;
+  esac
+done
+
+if [ -n "$httpaccess" ]; then
+  httpaccess=$(platform httpaccess -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} 2>&1)
+  # { grep deny || test $? = 1; } from https://stackoverflow.com/a/49627999
+  blocked_ips=($(echo "$httpaccess" | { grep deny || test $? = 1; }  | perl -pe "s/.*?address: '?([^\s\/]*).*/\$1/" | xargs -I {} echo '{}' | xargs))
+  blocked_already=$(echo "$httpaccess" | { grep deny || test $? = 1; }  | perl -pe "s/.*?address: ([^\s]*).*/\$1/" | xargs -I {} echo '--access deny:{}' | xargs)
+fi
+
 # The first grep is a whitelist, the second
 cmd_extra="grep -Pi '${PLATFORMSH_RECIPES_IPBLOCK_BLACKLIST//\'/\'\"\'\"\'}'"
 if [[ -n "$PLATFORMSH_RECIPES_IPBLOCK_WHITELIST" ]]; then
   cmd_extra="grep -vPi '${PLATFORMSH_RECIPES_IPBLOCK_WHITELIST//\'/\'\"\'\"\'}' | $cmd_extra"
 fi
-cmd_str="ahoy platform log:access --ip --extra '${cmd_extra//\'/\'\"\'\"\'}' $@"
+
+cmd_ip=--ip
+if [ -n "$ip" ]; then
+  cmd_ip="--ip=$ip --raw"
+fi
+
+cmd_str="ahoy platform log:access $days $cmd_ip --extra '${cmd_extra//\'/\'\"\'\"\'}' $@"
 >&2 echo -e "\033[0;36mRunning [ $cmd_str ]...\033[0m"
 ips=$(eval $cmd_str 2> /tmp/platformsh-recipes.ipblock.stderr)
 >&2 echo $(</tmp/platformsh-recipes.ipblock.stderr)
+
+if [ -n "$ip" ]; then
+  echo "$ips"
+  exit
+fi
+
+if [ -n "$iponly" ]; then
+  echo "$ips" | awk '{ if ($1 > '${PLATFORMSH_RECIPES_IPBLOCK_THRESHOLD-20}') print $1 "," $2}'
+  exit
+fi
+
 OLDIFS=$IFS
 IFS=$'\n'
 ips_array=($ips)
@@ -38,7 +90,6 @@ echo ""
 
 whitelist=
 whitelist_filename=$PLATFORM_APP_DIR/.deploy/.platformsh-recipes/ipblock.whitelist
-echo "$whitelist_filename"
 if [ -f "$whitelist_filename" ]; then
   whitelist=$(cat "$whitelist_filename" | grep -v -e "^#")
 fi
--- platformsh-recipes/scripts/logs/ipblock.sh
+++ platformsh-recipes/scripts/logs/ipblock.sh
@@ -16,15 +16,23 @@ if [[ -z "$PLATFORMSH_RECIPES_IPBLOCK_WHITELIST" ]]; then
   >&2 echo -e "\033[0;33mWarning: PLATFORMSH_RECIPES_IPBLOCK_WHITELIST not available, this could be fine.\033[0m"
 fi
 
-OPTIONS=`getopt -o '' -l ips-only,days: -- "$@"`
+OPTIONS=`getopt -o '' -l ips-only,days:,ip: -- "$@"`
 eval set -- "$OPTIONS"
 
 iponly=
+ip=
 days=
+httpaccess=1
 # extract options and their arguments into variables.
 while true ; do
   case "$1" in
+    --ip)
+      httpaccess=
+      ip=$2
+      shift 2
+      ;;
     --ips-only)
+      httpaccess=
       iponly=1
       shift
       ;;
@@ -40,7 +48,7 @@ while true ; do
   esac
 done
 
-if [ -z "$iponly" ]; then
+if [ -n "$httpaccess" ]; then
   httpaccess=$(platform httpaccess -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} 2>&1)
   # { grep deny || test $? = 1; } from https://stackoverflow.com/a/49627999
   blocked_ips=($(echo "$httpaccess" | { grep deny || test $? = 1; }  | perl -pe "s/.*?address: '?([^\s\/]*).*/\$1/" | xargs -I {} echo '{}' | xargs))
@@ -52,10 +60,21 @@ cmd_extra="grep -Pi '${PLATFORMSH_RECIPES_IPBLOCK_BLACKLIST//\'/\'\"\'\"\'}'"
 if [[ -n "$PLATFORMSH_RECIPES_IPBLOCK_WHITELIST" ]]; then
   cmd_extra="grep -vPi '${PLATFORMSH_RECIPES_IPBLOCK_WHITELIST//\'/\'\"\'\"\'}' | $cmd_extra"
 fi
-cmd_str="ahoy platform log:access $days --ip --extra '${cmd_extra//\'/\'\"\'\"\'}' $@"
+
+cmd_ip=--ip
+if [ -n "$ip" ]; then
+  cmd_ip="--ip=$ip --raw"
+fi
+
+cmd_str="ahoy platform log:access $days $cmd_ip --extra '${cmd_extra//\'/\'\"\'\"\'}' $@"
 >&2 echo -e "\033[0;36mRunning [ $cmd_str ]...\033[0m"
 ips=$(eval $cmd_str 2> /tmp/platformsh-recipes.ipblock.stderr)
 >&2 echo $(</tmp/platformsh-recipes.ipblock.stderr)
+
+if [ -n "$ip" ]; then
+  echo "$ips"
+  exit
+fi
 
 if [ -n "$iponly" ]; then
   echo "$ips" | awk '{ if ($1 > '${PLATFORMSH_RECIPES_IPBLOCK_THRESHOLD-20}') print $1 "," $2}'
