a7ec78304ea6fa408f63b65541a4848a349f02b8
Ariel Barreiro <abarrei@gmail.com>
2025-06-12 19:52:58 -0300

    feat: improve php-rpm for per seconds and other goodies, ipblock improvement too

--- platformsh-recipes/scripts/logs/ipblock.sh
+++ platformsh-recipes/scripts/logs/ipblock.sh
@@ -16,13 +16,15 @@ if [[ -z "$PLATFORMSH_RECIPES_IPBLOCK_WHITELIST" ]]; then
   >&2 echo -e "\033[0;33mWarning: PLATFORMSH_RECIPES_IPBLOCK_WHITELIST not available, this could be fine.\033[0m"
 fi
 
-OPTIONS=`getopt -o '' -l ips-only,days:,ip: -- "$@"`
+OPTIONS=`getopt -o '' -l ips-only,days:,ip:,all,no-block -- "$@"`
 eval set -- "$OPTIONS"
 
 iponly=
 ip=
 days=
 httpaccess=1
+noblock=0
+
 # extract options and their arguments into variables.
 while true ; do
   case "$1" in
@@ -34,6 +36,15 @@ while true ; do
     --ips-only)
       httpaccess=
       iponly=1
+      shift
+      ;;
+    --all)
+      days="--all"
+      shift
+      ;;
+    --no-block)
+      httpaccess=
+      noblock=1
       shift
       ;;
     --days)
@@ -71,19 +82,25 @@ cmd_str="ahoy platform log:access $days $cmd_ip --extra '${cmd_extra//\'/\'\"\'\
 ips=$(eval $cmd_str 2> /tmp/platformsh-recipes.ipblock.stderr)
 >&2 echo $(</tmp/platformsh-recipes.ipblock.stderr)
 
+cmd_str="ahoy platform log:php:rpm $days $cmd_ip --per-second --greater-than 50 --ips-only --all | awk '{ print \"1000000\", \$0 }'"
+>&2 echo -e "\033[0;36mRunning [ $cmd_str ]...\033[0m"
+ips2=$(eval $cmd_str 2> /tmp/platformsh-recipes.ipblock.stderr)
+>&2 echo $(</tmp/platformsh-recipes.ipblock.stderr)
+
 if [ -n "$ip" ]; then
   echo "$ips"
   exit
 fi
 
 if [ -n "$iponly" ]; then
-  echo "$ips" | awk '{ if ($1 > '${PLATFORMSH_RECIPES_IPBLOCK_THRESHOLD-20}') print $1 "," $2}'
+  echo "$ips" | awk '{ if ($1 > '${PLATFORMSH_RECIPES_IPBLOCK_THRESHOLD-20}') print $1 "," $2 ",bad" }'
+  echo "$ips2" | awk '{ if ($1 > '${PLATFORMSH_RECIPES_IPBLOCK_THRESHOLD-20}') print $1 "," $2 ",rate" }'
   exit
 fi
 
 OLDIFS=$IFS
 IFS=$'\n'
-ips_array=($ips)
+ips_array=($ips $ips2)
 IFS=$OLDIFS
 block=""
 echo ""
@@ -98,26 +115,31 @@ for i in ${!ips_array[*]}; do
   ip_cnt=(${ips_array[$i]})
   if [ ${ip_cnt[0]} -gt ${PLATFORMSH_RECIPES_IPBLOCK_THRESHOLD-20} ]; then
     ip=${ip_cnt[1]//[[:space:]]/}
-    if [[ ! "${blocked_ips[@]}" =~ $ip ]]; then
-      if [[ "${whitelist[@]}" =~ $ip ]]; then
-        >&2 echo -e "\033[0;33m[ warn  ] Bad IP $ip found with ${ip_cnt[0]} hits, but it is whitelisted...\033[0m"
-        continue
-      fi
-      >&2 echo -e "\033[0;33m[       ] Bad IP $ip found with ${ip_cnt[0]} hits, about to be blocked...\033[0m"
-      if [[ "$ip" =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ ]]; then
-        block="$block --access deny:$ip/32"
+    if [[ ! "$block" =~ "$ip" ]]; then
+      if [[ ! "${blocked_ips[@]}" =~ $ip ]]; then
+        if [[ "${whitelist[@]}" =~ $ip ]]; then
+          >&2 echo -e "\033[0;33m[ warn  ] Bad IP $ip found with ${ip_cnt[0]} hits, but it is whitelisted...\033[0m"
+          continue
+        fi
+        >&2 echo -e "\033[0;33m[       ] Bad IP $ip found with ${ip_cnt[0]} hits, about to be blocked...\033[0m"
+        if [[ "$ip" =~ [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+          block="$block --access deny:$ip/32"
+        else
+          block="$block --access deny:$ip/128"
+        fi
       else
-        block="$block --access deny:$ip/128"
+        if [[ "${whitelist[@]}" =~ $ip ]]; then
+          >&2 echo -e "\033[0;31m[ error ] Bad IP $ip found with ${ip_cnt[0]} hits was blocked, but it is now whitelisted, please remove.\033[0m"
+        fi
+        >&2 echo -e "\033[0;32m[ ok    ] Bad IP $ip found with ${ip_cnt[0]} hits, but already blocked...\033[0m"
       fi
     else
-      if [[ "${whitelist[@]}" =~ $ip ]]; then
-        >&2 echo -e "\033[0;31m[ error ] Bad IP $ip found with ${ip_cnt[0]} hits was blocked, but it is now whitelisted, please remove.\033[0m"
-      fi
       >&2 echo -e "\033[0;32m[ ok    ] Bad IP $ip found with ${ip_cnt[0]} hits, but already blocked...\033[0m"
     fi
   fi
 done
-if [ -n "$block" ]; then
+
+if [ $noblock -eq 0 -a -n "$block" ]; then
   if [ -z "$PLATFORM_APPLICATION_NAME" ]; then
     >&2 echo -e "\n\nIf you want to block the ips, run the following...\n"
     >&2 echo -e "platform httpaccess -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} \\\\\n\t\t\\\\\n\t\t$blocked_already \\\\\n\t\t\\\\\n\t\t$block"
--- platformsh-recipes/scripts/logs/php-rpm.sh
+++ platformsh-recipes/scripts/logs/php-rpm.sh
@@ -2,13 +2,9 @@
 #ddev-generated
 set -e
 
-OPTIONS=`getopt -o '' -l per-hour,hours:,days:,404,not-404,all,ip::,extra: -- "$@"`
+OPTIONS=`getopt -o '' -l greater-than:,per-second,per-hour,hours:,days:,404,not-404,all,ips-only,ip::,extra: -- "$@"`
 eval set -- "$OPTIONS"
-
-date1=$(php -r "print gmdate('^Y-m-d\TH', strtotime('now'));")
-date2=$(php -r "print gmdate('^Y-m-d\TH', strtotime('-1 hours'));")
-date3=$(php -r "print gmdate('^Y-m-d\TH', strtotime('-2 hours'));")
-grep_date='| grep -a -E "'"$date1|$date2|$date3"'"'
+grep_date=''
 grep_extra=""
 
 # $1  date
@@ -30,38 +26,43 @@ perl_other=' (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) (\S+) ([^\s\?]+)(?:(\?[^\s]+))?
 perl_show='\$1'
 perl_end='/"'
 pipe_extra=
-time=now
+per_hour=0
+per_minute=1
+per_second=0
+# Default to looking back 3 hours
+hours_back=3
+hours_offset=0
+grater_than=0
+
+ips_only=0
+all=0
+
 # extract options and their arguments into variables.
 while true ; do
   case "$1" in
+    --greater-than)
+      grater_than=$2
+      shift 2 ;;
+    --per-second)
+      per_hour=0
+      per_minute=0
+      per_second=1
+      shift;;
     --per-hour)
-      date=$(php -r "print gmdate('^Y-m-d', strtotime('$time'));")
-      grep_date='| grep -a -e "'"$date"'"'
-      perl_date='(^\d*[^:]*).*?'
+      per_second=0
+      per_minute=0
       per_hour=1
       shift;;
     --hours)
-      case "$2" in
-        "") time=now ; shift 2 ;;
-        *)
-          date1=$(php -r "print gmdate('^Y-m-d\TH', strtotime('-$2 hours'));")
-          date2=$(php -r "print gmdate('^Y-m-d\TH', strtotime('-$(($2 + 1)) hours'));")
-          date3=$(php -r "print gmdate('^Y-m-d\TH', strtotime('-$(($2 + 2)) hours'));")
-          grep_date='| grep -a -E "'"$date1|$date2|$date3"'"'
-          shift 2 ;;
-      esac ;;
+      hours_back=$2
+      shift 2 ;;
     --days)
-      if [ -z $per_hour -a -z $allday ]; then
-        echo "--days only can be used after --per-hour" ; exit 1
-      fi
-      case "$2" in
-        "") shift 2 ;;
-        *)
-          time="-$2 days"
-          date=$(php -r "print gmdate('^Y-m-d', strtotime('$time'));")
-          grep_date='| grep -a -e "'"$date"'"'
-          shift 2 ;;
-      esac ;;
+      hours_offset="$(($2*24))"
+      days_offset=$2
+      shift 2 ;;
+    --ips-only)
+      ips_only=1
+      shift ;;
     --ip)
       perl_show='\$1 - \$11'
       case "$2" in
@@ -71,7 +72,8 @@ while true ; do
           shift 2 ;;
       esac ;;
     --all)
-      grep_date='' ; shift ;;
+      all=1
+      shift ;;
     --404)
         grep_extra='| grep -a " 404 "' ; shift ;;
     --not-404)
@@ -84,15 +86,47 @@ while true ; do
   esac
 done
 
+if [ $per_second -eq 1 ]; then
+  if [ $all -eq 0 ]; then
+    dates=()
+    for ((i=0; i<hours_back; i++)); do
+      dates+=("$(php -r "print gmdate('^Y-m-d\TH', strtotime('-$(($hours_offset + $i)) hours'));")")
+    done
+    grep_date='| grep -a -E "'"$(IFS="|"; echo "${dates[*]}")"'"'
+  fi
+  perl_date='(^\d*[^:]*:[^:]*:.*?)'
+fi
+
+
+if [ $per_minute -eq 1 ]; then
+  if [ $all -eq 0 ]; then
+    dates=()
+    for ((i=0; i<hours_back; i++)); do
+      dates+=("$(php -r "print gmdate('^Y-m-d\TH', strtotime('-$(($hours_offset + $i)) hours'));")")
+    done
+    grep_date='| grep -a -E "'"$(IFS="|"; echo "${dates[*]}")"'"'
+  fi
+fi
+
+if [ $per_hour -eq 1 ]; then
+  if [ $all -eq 0 ]; then
+    date=$(php -r "print gmdate('^Y-m-d', strtotime('-$days_offset days'));")
+    grep_date='| grep -a -e "'"$date"'"'
+  fi
+  perl_date='(^\d*[^:]*).*?'
+fi
+
 PHP_ACCESS_LOG_FILEPATH="/var/log/php.access.log"
 if [ -n "$PLATFORMSH_RECIPES_DEV" ]; then
   PHP_ACCESS_LOG_FILEPATH="php.access.log"
 fi
 cmd='cat '"$PHP_ACCESS_LOG_FILEPATH"' '"$grep_date"' '"$grep_extra"' '"$pipe_extra"' '"$grep_before"' '"$perl_start$perl_date$perl_other$perl_show$perl_end"' '"$grep_after"' | sort | uniq -c'
-awk='awk '"'"'{sum += $1; print} END {print "\033[1;35m\n>>", sum, "total <<\n\033[0m" > "/dev/stderr"}'"'"
->&2 printf "\033[0;36mRunning [ %s | %s ]...\033[0m\n" "$cmd" "$awk"
+gt=${GT_THRESHOLD:-$grater_than}
+awk='awk -v gt="'$gt'" -v ips_only="'$ips_only'" '"'"'{if ($1 > gt) {sum += $1; print}} END {if (sum > 0 && ips_only != 1) print "\033[1;35m\n>>", sum, "total <<\n\033[0m" > "/dev/stderr"}'"'"
+[ "$ips_only" = "1" ] && unique_ips_filter='| awk "{print \$4}" | sort | uniq' || unique_ips_filter=''
+>&2 printf "\033[0;36mRunning [ %s | %s %s ]...\033[0m\n" "$cmd" "$awk" "$unique_ips_filter"
 if [ -z "$PLATFORM_APPLICATION_NAME" -a -z "$PLATFORMSH_RECIPES_DEV" ]; then
-  platform ssh -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} "$cmd" | eval $awk
+  platform ssh -e ${PLATFORMSH_RECIPES_MAIN_BRANCH-master} "$cmd" | eval $awk $unique_ips_filter
 else
-  eval $cmd | eval $awk
+  eval $cmd | eval $awk $unique_ips_filter
 fi
